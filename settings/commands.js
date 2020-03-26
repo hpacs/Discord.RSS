fb8083701e5e72dea32f7bccd9095eae464cb2be
hpacs <46070954+hpacs@users.noreply.github.com>
2020-03-26 08:46:44 +0800

    Update commands.js

--- settings/commands.js
+++ settings/commands.js
@@ -40,6 +40,22 @@ const getServer = async sgame =>{
 
 module.exports = function (bot, message) {
   if (message.author.bot) return;
+  else if (message.content.startswith("/inv") {
+    if(!(message.member.roles.find(r => r.name === "Moderator") || message.member.roles.find(r => rname === "Asst Mod"))) return;
+    async function replyWithInvite(message) {
+  let invite = await message.channel.createInvite(
+  {
+    maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
+    maxUses: 1 // maximum times it can be used
+  },
+  `Requested with command by ${message.author.tag}`
+)
+.catch(console.log);
+
+  message.reply(invite ? `Here's your invite: ${invite}` : "There has been an error during the creation of the invite.");
+}
+           
+           }
   else if (message.content.startsWith("/kom")) {
       var prom1 = getData(url);
       prom1.then(function(msg){
